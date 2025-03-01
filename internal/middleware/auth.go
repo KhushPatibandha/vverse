b1d5e94503dda2a702cab649f28695faabbbfc0c
KhushPatibandha <khush.patibandha@gmail.com>
2025-03-01 13:45:47 +0530

    super secure auth added

---
+++ internal/middleware/auth.go
@@ -0,0 +1,36 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/KhushPatibandha/vverse/api"
+)
+
+var UnAuthError = errors.New("Invalid token.")
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// > All API calls must be authenticated (assume static API tokens)
+		// In case of actual implementation, connect to the database to verify the token, also use JWT
+		token := r.Header.Get("Authorization")
+		if token == "" {
+			log.Error(UnAuthError)
+			api.RequestErrorHandler(w, UnAuthError)
+			return
+		}
+
+		// simulate fetching token from database
+		time.Sleep(time.Second * 2)
+
+		if token != "someCrazySecureToken" {
+			log.Error(UnAuthError)
+			api.RequestErrorHandler(w, UnAuthError)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
